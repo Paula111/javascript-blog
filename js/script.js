331a66adcafc0167cc46b778f554001178655a16
Paulina Babinczuk <paulina.babinczuk@gmail.com>
2020-11-03 11:07:14 +0100

    delete links in index.html upload links in script.js

--- js/script.js
+++ js/script.js
@@ -43,7 +43,54 @@ function titleClickHandler(event) {
 
 }
 
+
+
+/* GENERATE TITLE LIST */
+
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+function generateTitleLinks() {
+    console.log(generateTitleLinks);
+
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = "";
+
+    const articles = document.querySelectorAll(optArticleSelector);
+    let html = "";
+
+    /* for each article */
+    for (let article of articles) {
+
+        /* get the article id */
+        const articleId = article.getAttribute('id');
+
+        /* find the title element and get the title from the title element*/
+
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+
+        /* create HTML of the link */
+
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+        console.log(linkHTML);
+
+
+        /* insert link into titleList */
+
+        html = html + linkHTML;
+        console.log(html);
+    }
+
+    titleList.innerHTML = html;
+}
+
+
+generateTitleLinks();
+
 const links = document.querySelectorAll('.titles a');
+console.log(links);
 
 for (let link of links) {
     link.addEventListener('click', titleClickHandler);
