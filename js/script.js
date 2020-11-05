36e54ae7c1921b822f55ffa6e1fd6aebfd3f805e
Paulina Babinczuk <paulina.babinczuk@gmail.com>
2020-11-05 09:14:28 +0100

    correct file js/script.js

--- js/script.js
+++ js/script.js
@@ -84,14 +84,13 @@ function generateTitleLinks() {
     }
 
     titleList.innerHTML = html;
-}
 
+    const links = document.querySelectorAll('.titles a');
+    console.log(links);
+
+    for (let link of links) {
+        link.addEventListener('click', titleClickHandler);
+    }
+}
 
 generateTitleLinks();
-
-const links = document.querySelectorAll('.titles a');
-console.log(links);
-
-for (let link of links) {
-    link.addEventListener('click', titleClickHandler);
-}
